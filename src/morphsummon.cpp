4786a393333f902e43ca34528fe514421e4a1428
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2020-02-04 11:57:19 +0100

    Various updates

--- src/morphsummon.cpp
+++ src/morphsummon.cpp
@@ -14,7 +14,9 @@
 std::map<std::string, uint32> warlock_imp;
 std::map<std::string, uint32> warlock_voidwalker;
 std::map<std::string, uint32> warlock_succubus;
+std::map<std::string, uint32> warlock_felhunter;
 std::map<std::string, uint32> warlock_felguard;
+std::map<std::string, uint32> felguard_weapon;
 std::map<std::string, uint32> death_knight_ghoul;
 std::map<std::string, uint32> mage_water_elemental;
 std::list<uint32> randomVisualEffectSpells;
@@ -29,10 +31,12 @@ enum MorphSummonGossip
     MORPH_PAGE_START_WARLOCK_IMP          =    101,
     MORPH_PAGE_START_WARLOCK_VOIDWALKER   =    201,
     MORPH_PAGE_START_WARLOCK_SUCCUBUS     =    301,
-    MORPH_PAGE_START_WARLOCK_FELGUARD     =    401,
-    MORPH_PAGE_START_DEATH_KNIGHT_GHOUL   =    501,
-    MORPH_PAGE_START_MAGE_WATER_ELEMENTAL =    601,
-    MORPH_PAGE_MAX                        =    701,
+    MORPH_PAGE_START_WARLOCK_FELHUNTER    =    401,
+    MORPH_PAGE_START_WARLOCK_FELGUARD     =    501,
+    MORPH_PAGE_START_DEATH_KNIGHT_GHOUL   =    601,
+    MORPH_PAGE_START_MAGE_WATER_ELEMENTAL =    701,
+    MORPH_PAGE_START_FELGUARD_WEAPON      =    801,
+    MORPH_PAGE_MAX                        =    901,
     MORPH_MAIN_MENU                       =     50,
     MORPH_CLOSE_MENU                      =     60,
     MORPH_GOSSIP_TEXT_HELLO               = 601072,
@@ -41,7 +45,8 @@ enum MorphSummonGossip
     MORPH_GOSSIP_MENU_HELLO               =  61072,
     MORPH_GOSSIP_MENU_SORRY               =  61073,
     MORPH_GOSSIP_MENU_CHOICE              =  61074,
-    MORPH_GOSSIP_OPTION_HELLO             =      0,
+    MORPH_GOSSIP_OPTION_POLYMORPH         =      0,
+    MORPH_GOSSIP_OPTION_FELGUARD_WEAPON   =      1,
     MORPH_GOSSIP_OPTION_SORRY             =      0,
     MORPH_GOSSIP_OPTION_CHOICE_BACK       =      0,
     MORPH_GOSSIP_OPTION_CHOICE_NEXT       =      1,
@@ -53,6 +58,7 @@ enum MorphSummonSpells
     SUMMON_IMP                            =    688,
     SUMMON_VOIDWALKER                     =    697,
     SUMMON_SUCCUBUS                       =    712,
+    SUMMON_FELHUNTER                      =    691,
     SUMMON_FELGUARD                       =  30146,
     RAISE_DEAD                            =  52150,
     SUMMON_WATER_ELEMENTAL                =  70908
@@ -82,13 +88,24 @@ public:
         }
     }
 
-    void OnPetInitStatsForLevel(Pet* pet) override
+    void OnAfterGuardianInitStatsForLevel(Player* player, Guardian* guardian) override
     {
-        if (pet->GetUInt32Value(UNIT_CREATED_BY_SPELL) == SUMMON_WATER_ELEMENTAL)
+        if (Pet* pet = guardian->ToPet())
         {
-            // The size of the water elemental model is not automatically scaled, so needs to be done here
-            CreatureDisplayInfoEntry const* displayInfo = sCreatureDisplayInfoStore.LookupEntry(pet->GetNativeDisplayId());
-            pet->SetObjectScale(0.85f / displayInfo->scale);
+            if (pet->GetUInt32Value(UNIT_CREATED_BY_SPELL) == SUMMON_WATER_ELEMENTAL)
+            {
+                // The size of the water elemental model is not automatically scaled, so needs to be done here
+                CreatureDisplayInfoEntry const* displayInfo = sCreatureDisplayInfoStore.LookupEntry(pet->GetNativeDisplayId());
+                pet->SetObjectScale(0.85f / displayInfo->scale);
+            }
+            else if (pet->GetUInt32Value(UNIT_CREATED_BY_SPELL) == SUMMON_FELGUARD)
+            {
+                if (QueryResult result = CharacterDatabase.PQuery("SELECT `FelguardItemID` FROM `mod_morphsummon_felguard_weapon` WHERE `PlayerGUIDLow` = %u", player->GetGUIDLow()))
+                {
+                    Field* fields = result->Fetch();
+                    pet->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID, fields[0].GetUInt32());
+                }
+            }
         }
     }
 };
@@ -124,9 +141,13 @@ public:
         {
             AddGossip(player, action, warlock_voidwalker, MORPH_PAGE_START_WARLOCK_VOIDWALKER);
         }
-        else if (action >= MORPH_PAGE_START_WARLOCK_SUCCUBUS && action < MORPH_PAGE_START_WARLOCK_FELGUARD)
+        else if (action >= MORPH_PAGE_START_WARLOCK_SUCCUBUS && action < MORPH_PAGE_START_WARLOCK_FELHUNTER)
         {
             AddGossip(player, action, warlock_succubus, MORPH_PAGE_START_WARLOCK_SUCCUBUS);
+        }
+        else if (action >= MORPH_PAGE_START_WARLOCK_FELHUNTER && action < MORPH_PAGE_START_WARLOCK_FELGUARD)
+        {
+            AddGossip(player, action, warlock_felhunter, MORPH_PAGE_START_WARLOCK_FELHUNTER);
         }
         else if (action >= MORPH_PAGE_START_WARLOCK_FELGUARD && action < MORPH_PAGE_START_DEATH_KNIGHT_GHOUL)
         {
@@ -136,35 +157,47 @@ public:
         {
             AddGossip(player, action, death_knight_ghoul, MORPH_PAGE_START_DEATH_KNIGHT_GHOUL);
         }
-        else if (action >= MORPH_PAGE_START_MAGE_WATER_ELEMENTAL && action < MORPH_PAGE_MAX)
+        else if (action >= MORPH_PAGE_START_MAGE_WATER_ELEMENTAL && action < MORPH_PAGE_START_FELGUARD_WEAPON)
         {
             AddGossip(player, action, mage_water_elemental, MORPH_PAGE_START_MAGE_WATER_ELEMENTAL);
+        }
+        else if (action >= MORPH_PAGE_START_FELGUARD_WEAPON && action < MORPH_PAGE_MAX)
+        {
+            AddGossip(player, action, felguard_weapon, MORPH_PAGE_START_FELGUARD_WEAPON);
         }
         else if (action >= MORPH_PAGE_MAX)
         {
             if (sender >= MORPH_PAGE_START_WARLOCK_IMP && sender < MORPH_PAGE_START_WARLOCK_VOIDWALKER)
             {
-                Polymorph(player, action, sender, MORPH_PAGE_START_WARLOCK_IMP, MORPH_PAGE_START_WARLOCK_VOIDWALKER, SUMMON_IMP, warlock_imp);
+                Polymorph(player, action, sender, MORPH_PAGE_START_WARLOCK_IMP, MORPH_PAGE_START_WARLOCK_VOIDWALKER, SUMMON_IMP, warlock_imp, true);
             }
             else if (sender >= MORPH_PAGE_START_WARLOCK_VOIDWALKER && sender < MORPH_PAGE_START_WARLOCK_SUCCUBUS)
             {
-                Polymorph(player, action, sender, MORPH_PAGE_START_WARLOCK_VOIDWALKER, MORPH_PAGE_START_WARLOCK_SUCCUBUS, SUMMON_VOIDWALKER, warlock_voidwalker);
+                Polymorph(player, action, sender, MORPH_PAGE_START_WARLOCK_VOIDWALKER, MORPH_PAGE_START_WARLOCK_SUCCUBUS, SUMMON_VOIDWALKER, warlock_voidwalker, true);
             }
-            else if (sender >= MORPH_PAGE_START_WARLOCK_SUCCUBUS && sender < MORPH_PAGE_START_WARLOCK_FELGUARD)
+            else if (sender >= MORPH_PAGE_START_WARLOCK_SUCCUBUS && sender < MORPH_PAGE_START_WARLOCK_FELHUNTER)
             {
-                Polymorph(player, action, sender, MORPH_PAGE_START_WARLOCK_SUCCUBUS, MORPH_PAGE_START_WARLOCK_FELGUARD, SUMMON_SUCCUBUS, warlock_succubus);
+                Polymorph(player, action, sender, MORPH_PAGE_START_WARLOCK_SUCCUBUS, MORPH_PAGE_START_WARLOCK_FELHUNTER, SUMMON_SUCCUBUS, warlock_succubus, true);
+            }
+            else if (sender >= MORPH_PAGE_START_WARLOCK_FELHUNTER && sender < MORPH_PAGE_START_WARLOCK_FELGUARD)
+            {
+                Polymorph(player, action, sender, MORPH_PAGE_START_WARLOCK_FELHUNTER, MORPH_PAGE_START_WARLOCK_FELGUARD, SUMMON_FELHUNTER, warlock_felhunter, true);
             }
             else if (sender >= MORPH_PAGE_START_WARLOCK_FELGUARD && sender < MORPH_PAGE_START_DEATH_KNIGHT_GHOUL)
             {
-                Polymorph(player, action, sender, MORPH_PAGE_START_WARLOCK_FELGUARD, MORPH_PAGE_START_DEATH_KNIGHT_GHOUL, SUMMON_FELGUARD, warlock_felguard);
+                Polymorph(player, action, sender, MORPH_PAGE_START_WARLOCK_FELGUARD, MORPH_PAGE_START_DEATH_KNIGHT_GHOUL, SUMMON_FELGUARD, warlock_felguard, true);
             }
             else if (sender >= MORPH_PAGE_START_DEATH_KNIGHT_GHOUL && sender < MORPH_PAGE_START_MAGE_WATER_ELEMENTAL)
             {
-                Polymorph(player, action, sender, MORPH_PAGE_START_DEATH_KNIGHT_GHOUL, MORPH_PAGE_START_MAGE_WATER_ELEMENTAL, RAISE_DEAD, death_knight_ghoul);
+                Polymorph(player, action, sender, MORPH_PAGE_START_DEATH_KNIGHT_GHOUL, MORPH_PAGE_START_MAGE_WATER_ELEMENTAL, RAISE_DEAD, death_knight_ghoul, true);
             }
-            else if (sender >= MORPH_PAGE_START_MAGE_WATER_ELEMENTAL && sender < MORPH_PAGE_MAX)
+            else if (sender >= MORPH_PAGE_START_MAGE_WATER_ELEMENTAL && sender < MORPH_PAGE_START_FELGUARD_WEAPON)
             {
-                Polymorph(player, action, sender, MORPH_PAGE_START_MAGE_WATER_ELEMENTAL, MORPH_PAGE_MAX, SUMMON_WATER_ELEMENTAL, mage_water_elemental);
+                Polymorph(player, action, sender, MORPH_PAGE_START_MAGE_WATER_ELEMENTAL, MORPH_PAGE_START_FELGUARD_WEAPON, SUMMON_WATER_ELEMENTAL, mage_water_elemental, true);
+            }
+            else if (sender >= MORPH_PAGE_START_FELGUARD_WEAPON && sender < MORPH_PAGE_MAX)
+            {
+                Polymorph(player, action, sender, MORPH_PAGE_START_FELGUARD_WEAPON, MORPH_PAGE_MAX, SUMMON_FELGUARD, felguard_weapon, false);
             }
         }
 
@@ -227,37 +260,54 @@ private:
             {
                 case SUMMON_IMP:
                     if (!warlock_imp.empty())
-                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_HELLO, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_WARLOCK_IMP);
+                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_POLYMORPH, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_WARLOCK_IMP);
                     else
                         sorry = true;
                     break;
                 case SUMMON_VOIDWALKER:
                     if (!warlock_voidwalker.empty())
-                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_HELLO, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_WARLOCK_VOIDWALKER);
+                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_POLYMORPH, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_WARLOCK_VOIDWALKER);
                     else
                         sorry = true;
                     break;
                 case SUMMON_SUCCUBUS:
                     if (!warlock_succubus.empty())
-                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_HELLO, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_WARLOCK_SUCCUBUS);
+                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_POLYMORPH, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_WARLOCK_SUCCUBUS);
+                    else
+                        sorry = true;
+                    break;
+                case SUMMON_FELHUNTER:
+                    if (!warlock_felhunter.empty())
+                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_POLYMORPH, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_WARLOCK_FELHUNTER);
                     else
                         sorry = true;
                     break;
                 case SUMMON_FELGUARD:
                     if (!warlock_felguard.empty())
-                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_HELLO, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_WARLOCK_FELGUARD);
+                    {
+                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_POLYMORPH, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_WARLOCK_FELGUARD);
+
+                        if (!felguard_weapon.empty())
+                        {
+                            AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_FELGUARD_WEAPON, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_FELGUARD_WEAPON);
+                        }
+                    }
+                    else if (!felguard_weapon.empty())
+                    {
+                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_FELGUARD_WEAPON, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_FELGUARD_WEAPON);
+                    }
                     else
                         sorry = true;
                     break;
                 case RAISE_DEAD:
                     if (!death_knight_ghoul.empty())
-                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_HELLO, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_DEATH_KNIGHT_GHOUL);
+                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_POLYMORPH, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_DEATH_KNIGHT_GHOUL);
                     else
                         sorry = true;
                     break;
                 case SUMMON_WATER_ELEMENTAL:
                     if (!mage_water_elemental.empty())
-                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_HELLO, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_MAGE_WATER_ELEMENTAL);
+                        AddGossipItemFor(player, MORPH_GOSSIP_MENU_HELLO, MORPH_GOSSIP_OPTION_POLYMORPH, GOSSIP_SENDER_MAIN, MORPH_PAGE_START_MAGE_WATER_ELEMENTAL);
                     else
                         sorry = true;
                     break;
@@ -304,7 +354,7 @@ private:
         }
     }
 
-    void Polymorph(Player* player, uint32 action, uint32 sender, uint32 startPage, uint32 maxPage, uint32 spell, std::map<std::string, uint32> &modelMap)
+    void Polymorph(Player* player, uint32 action, uint32 sender, uint32 startPage, uint32 maxPage, uint32 spell, std::map<std::string, uint32> &modelMap, bool polymorphPet)
     {
         if (Pet* pet = player->GetPet())
         {
@@ -312,19 +362,29 @@ private:
             {
                 if (pet->GetUInt32Value(UNIT_CREATED_BY_SPELL) == spell)
                 {
-                    pet->SetDisplayId(action - MORPH_PAGE_MAX);
-                    pet->SetNativeDisplayId(action - MORPH_PAGE_MAX);
+                    uint32 morphId = action - MORPH_PAGE_MAX;
 
-                    if (pet->GetUInt32Value(UNIT_CREATED_BY_SPELL) == SUMMON_WATER_ELEMENTAL)
+                    if (polymorphPet)
                     {
-                        // The size of the water elemental model is not automatically scaled, so needs to be done here
-                        CreatureDisplayInfoEntry const* displayInfo = sCreatureDisplayInfoStore.LookupEntry(pet->GetNativeDisplayId());
-                        pet->SetObjectScale(0.85f / displayInfo->scale);
-                    }
+                        pet->SetDisplayId(morphId);
+                        pet->SetNativeDisplayId(morphId);
 
-                    if (Aura* aura = pet->AddAura(SUBMERGE, pet))
-                        aura->SetDuration(2000);
-                    pet->CastSpell(pet, SHADOW_SUMMON_VISUAL, true);
+                        if (pet->GetUInt32Value(UNIT_CREATED_BY_SPELL) == SUMMON_WATER_ELEMENTAL)
+                        {
+                            // The size of the water elemental model is not automatically scaled, so needs to be done here
+                            CreatureDisplayInfoEntry const* displayInfo = sCreatureDisplayInfoStore.LookupEntry(pet->GetNativeDisplayId());
+                            pet->SetObjectScale(0.85f / displayInfo->scale);
+                        }
+
+                        if (Aura* aura = pet->AddAura(SUBMERGE, pet))
+                            aura->SetDuration(2000);
+                        pet->CastSpell(pet, SHADOW_SUMMON_VISUAL, true);
+                    }
+                    else
+                    {
+                        pet->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID, morphId);
+                        CharacterDatabase.PExecute("REPLACE INTO `mod_morphsummon_felguard_weapon` (`PlayerGUIDLow`, `FelguardItemID`) VALUES (%u, %u)", player->GetGUIDLow(), morphId);
+                    }
                 }
             }
 
@@ -369,14 +429,18 @@ public:
         warlock_imp.clear();
         warlock_voidwalker.clear();
         warlock_succubus.clear();
+        warlock_felhunter.clear();
         warlock_felguard.clear();
+        felguard_weapon.clear();
         death_knight_ghoul.clear();
         mage_water_elemental.clear();
 
         LoadModels(sConfigMgr->GetStringDefault("MorphSummon.Warlock.Imp", ""), warlock_imp);
         LoadModels(sConfigMgr->GetStringDefault("MorphSummon.Warlock.Voidwalker", ""), warlock_voidwalker);
         LoadModels(sConfigMgr->GetStringDefault("MorphSummon.Warlock.Succubus", ""), warlock_succubus);
+        LoadModels(sConfigMgr->GetStringDefault("MorphSummon.Warlock.Felhunter", ""), warlock_felhunter);
         LoadModels(sConfigMgr->GetStringDefault("MorphSummon.Warlock.Felguard", ""), warlock_felguard);
+        LoadModels(sConfigMgr->GetStringDefault("MorphSummon.Warlock.Felguard.Weapon", ""), felguard_weapon);
         LoadModels(sConfigMgr->GetStringDefault("MorphSummon.DeathKnight.Ghoul", ""), death_knight_ghoul);
         LoadModels(sConfigMgr->GetStringDefault("MorphSummon.Mage.WaterElemental", ""), mage_water_elemental);
     }
